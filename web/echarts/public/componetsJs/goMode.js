452209e5b74f237927ad3be2325efa4325116425
huanghu0 <1525273035@qq.com>
2020-01-10 16:43:43 +0800

    界面布局

---
+++ web/echarts/public/componetsJs/goMode.js
@@ -0,0 +1,45 @@
+var myChart = echarts.init(document.getElementById('goMode'));
+option = {
+    title: {
+        text: '市民出行方式',
+        left: 'center',
+        textStyle:{
+            color: '#0099FF',
+        }
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        textStyle:{
+            color: '#0099FF',
+        },
+        data: ['步行', '地铁', '公交', '私家车', '自行车']
+    },
+    series: [
+        {
+            name: '出行方式',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value: 335, name: '步行'},
+                {value: 310, name: '地铁'},
+                {value: 234, name: '公交'},
+                {value: 135, name: '私家车'},
+                {value: 1548, name: '自行车'}
+            ],
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+myChart.setOption(option);

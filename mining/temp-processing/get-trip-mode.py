6572874d83a7939319dea969a4297a82dc6fbb1b
AngeeloLee <artisigngineer@hotmail.com>
2020-05-16 22:36:34 +0800

    更新数据

--- mining/temp-processing/get-trip-mode.py
+++ mining/temp-processing/get-trip-mode.py
@@ -17,11 +17,13 @@ with open('../../data/trip-mode.csv','r') as datafile:
             continue
         if(line[5] == 'T'):
             if(line[2] == imsi and line[6] == mode):
+                coords.append([float(line[3]), float(line[4])])
                 jsondata.append({'imsi':imsi, 'mode':modes[int(mode)-1], 'coords':coords})
             else:
                 imsi = line[2]
                 mode = line[6]
                 coords = []
+                coords.append([float(line[3]), float(line[4])])
         else:
             coords.append([float(line[3]), float(line[4])])
     datafile.close()
